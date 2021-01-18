743f7ca0f3babd07a2c488b808eb6ae530ea9c70
Elena Slyusareva <sportgirl55@bk.ru>
2021-01-19 03:08:03 +0500

    Tablet version is added

---
+++ js/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+const messengers = document.querySelector('.messengers_header');
+
+
+
+
+
+// window.onload = function () {
+//   const hamb = document.querySelector('.hamb');
+//   const closeMenu = document.querySelector('.close-menu');
+//   const header = document.querySelector('.header-page');
+  
+//   hamb.addEventListener('click', () => {
+//       header.classList.add('open');
+//   });
+  
+//   closeMenu.addEventListener('click', () => {
+//       header.classList.remove('open');
+//   })
+  
+//   if (document.querySelector('.btn-scroll')) {
+//       const anchors = document.querySelector('.btn-scroll');
+  
+//       anchors.addEventListener('click', () => {
+//           const blockID = document.querySelector('.cars');
+      
+//           blockID.scrollIntoView({
+//               behavior: 'smooth',
+//               block: 'start'
+//           })
+//       })
+//   }
+// }
