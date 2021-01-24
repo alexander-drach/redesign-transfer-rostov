05f3bf7c98bc510df54ec5d453dc10712cd9ff0e
Alexander Drach <alexander-drach@mail.ru>
2021-01-24 16:11:21 +0600

    add social links

--- includes/social.php
+++ includes/social.php
@@ -1,24 +1,24 @@
 <li class="list__item">
-  <a class="social__link social__link_vk" href="#"><span class="visually-hidden">РостовЮгТрансфер в
+  <a class="social__link social__link_vk" href="https://vk.com/taxiizrostova"><span class="visually-hidden">РостовЮгТрансфер в
               вконтакте</span></a>
 </li><!-- list__item -->
 
 <li class="list__item">
-  <a class="social__link social__link_facebook" href="#"><span class="visually-hidden">РостовЮгТрансфер в
+  <a class="social__link social__link_facebook" href="https://www.facebook.com/taxiizrostova/"><span class="visually-hidden">РостовЮгТрансфер в
               facebook</span></a>
 </li><!-- list__item -->
 
 <li class="list__item">
-  <a class="social__link social__link_instagram" href="#"><span class="visually-hidden">РостовЮгТрансфер в
+  <a class="social__link social__link_instagram" href="https://www.instagram.com/taxiizrostova/"><span class="visually-hidden">РостовЮгТрансфер в
               instagram</span></a>
 </li><!-- list__item -->
 
 <li class="list__item">
-  <a class="social__link social__link_twitter" href="#"><span class="visually-hidden">РостовЮгТрансфер в
+  <a class="social__link social__link_twitter" href="https://taxiizrostova.ru/kontakty-taksi-iz-rostova-na-donu?share=twitter&amp;nb=1"><span class="visually-hidden">РостовЮгТрансфер в
               twitter</span></a>
 </li><!-- list__item -->
 
 <li class="list__item">
-  <a class="social__link social__link_youtube" href="#"><span class="visually-hidden">РостовЮгТрансфер на
+  <a class="social__link social__link_youtube" href="https://www.youtube.com/channel/UCQ0Cb7J-i53CSitctgaBZeQ/featured"><span class="visually-hidden">РостовЮгТрансфер на
               youtube</span></a>
 </li><!-- list__item -->
