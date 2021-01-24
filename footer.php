4e2e427d9ccd5a7d1d653ad596793b1707fdf566
Alexander Drach <alexander-drach@mail.ru>
2021-01-24 21:19:50 +0600

    add links footer menu

--- footer.php
+++ footer.php
@@ -17,12 +17,12 @@
 
       </ul>
       <ul class="list footer-menu">
-        <li><a href="#">Главная</a></li>
-        <li><a href="#">Услуги</a></li>
-        <li><a href="#">Направления</a></li>
-        <li><a href="#">Бронирование</a></li>
-        <li><a href="#">О нас</a></li>
-        <li><a href="#">Контакты</a></li>
+        <li><a href="index.php">Главная</a></li>
+        <li><a href="index.php#services">Услуги</a></li>
+        <li><a href="index.php#routs">Направления</a></li>
+        <li><a href="page-booking.php">Бронирование</a></li>
+        <li><a href="page-about.php">О нас</a></li>
+        <li><a href="index.php#contact">Контакты</a></li>
       </ul>
       <div class="copyright">
         <div class="law">
