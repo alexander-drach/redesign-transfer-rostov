b9bd929f11e3d4418f2c356f1b5d3b4e8813f045
Elena Slyusareva <sportgirl55@bk.ru>
2021-02-10 12:48:36 +0500

    Button 'scrollUp' is done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// HAMBURGER
+
 const hamb = document.querySelector('.hamb');
 const header = document.querySelector('.header-page');
 const closeMenu = document.querySelector('.close-menu');
@@ -14,8 +16,20 @@ closeMenu.addEventListener('click', function () {
     header.classList.remove('open');
 });
 
-menuItems.forEach( function (item) {
+menuItems.forEach(function (item) {
     item.addEventListener('click', function () {
         header.classList.remove('open');
+    })
+})
+
+// BUTTON UP
+
+const btnUp = document.querySelector('.btn-up');
+const rootElement = document.documentElement;
+
+btnUp.addEventListener("click", function () {
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
     })
 })
